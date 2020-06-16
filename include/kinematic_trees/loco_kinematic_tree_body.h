ae91b096d1d286e33d7a71d1d8eb3ea59e06085e
wpumacay <wpumacay@gmail.com>
2020-06-15 23:07:27 -0500

    [Add feat.]: kintree-body primitive helpers

--- include/kinematic_trees/loco_kinematic_tree_body.h
+++ include/kinematic_trees/loco_kinematic_tree_body.h
@@ -14,8 +14,18 @@ namespace visualizer {
 
 namespace loco {
 namespace kintree {
-
     struct TKinematicTreeJointData;
+}}
+
+namespace loco {
+namespace kintree {
+
+    //--------------------------------------------------------------------------------------------//
+    //                                                                                            //
+    //                               Kinematic-Tree-Body Interface                                //
+    //                                                                                            //
+    //--------------------------------------------------------------------------------------------//
+
     /// Struct with the details required to create a kinematic-tree body
     struct TKinematicTreeBodyData
     {
@@ -38,12 +48,12 @@ namespace kintree {
     {
     public :
 
-        TKinematicTreeBody( const std::string& name,
-                            const TKinematicTreeBodyData& body_data,
-                            const TVec3& position = TVec3(),
-                            const TMat3& rotation = TMat3() );
+        TKinematicTreeBody( const std::string& name );
 
-        ~TKinematicTreeBody();
+        TKinematicTreeBody( const std::string& name,
+                            const TKinematicTreeBodyData& body_data );
+
+        virtual ~TKinematicTreeBody();
 
         void SetBodyAdapter( TIKinematicTreeBodyAdapter* body_adapter_ref );
 
@@ -142,5 +152,284 @@ namespace kintree {
         TVec3 m_TotalTorque = { 0.0f, 0.0f, 0.0f };
 
         TMat4 m_LocalTf;
+    };
+
+    //--------------------------------------------------------------------------------------------//
+    //                                                                                            //
+    //                              Kinematic-Tree-Body Primitives                                //
+    //                                                                                            //
+    //--------------------------------------------------------------------------------------------//
+
+    class TBox : public TKinematicTreeBody
+    {
+    public :
+
+        TBox( const std::string& name,
+              const TVec3& extents,
+              const int& collision_group = 1,
+              const int& collision_mask = 1 );
+
+        TBox( const TBox& other ) = delete;
+
+        TBox& operator=( const TBox& other ) = delete;
+
+        ~TBox() = default;
+
+        /// Sets the width|depth|height extents of the box collider
+        ///
+        /// @param extents  Vector containing the witdh, depth, and height for the box collider
+        void SetExtents( const TVec3& extents );
+
+        /// Sets the width of the box collider
+        ///
+        /// @param width    Width of the box collider
+        void SetWidth( const TScalar& width );
+
+        /// Sets the depth of the box collider
+        ///
+        /// @param depth    Depth of the box collider
+        void SetDepth( const TScalar& depth );
+
+        /// Sets the height of the box collider
+        ///
+        /// @param height   Height of the box collider
+        void SetHeight( const TScalar& height );
+
+        /// Gets the extents of the box collider
+        ///
+        /// @return Vector containing the witdh, depth, and height for the box collider
+        TVec3 extents() const { return m_Extents; }
+
+        /// Gets the width of the box collider
+        ///
+        /// @return Width value of the box collider
+        TScalar width() const { return m_Extents.x(); }
+
+        /// Gets the depth of the box collider
+        ///
+        /// @return Depth value of the box collider
+        TScalar depth() const { return m_Extents.y(); }
+
+        /// Gets the height of the box collider
+        ///
+        /// @return Height value of the box collider
+        TScalar height() const { return m_Extents.z(); }
+
+    private :
+
+        /// Extents of the box-shaped collider|drawable
+        TVec3 m_Extents;
+    };
+
+    class TSphere : public TKinematicTreeBody
+    {
+    public :
+
+        TSphere( const std::string& name,
+                 const TScalar& radius,
+                 const int& collision_group = 1,
+                 const int& collision_mask = 1 );
+
+        TSphere( const TSphere& other ) = delete;
+
+        TSphere& operator=( const TSphere& other ) = delete;
+
+        ~TSphere() = default;
+
+        /// Sets the radius of the sphere-shaped collider|drawable
+        ///
+        /// @param radius   Radius of the sphere
+        void SetRadius( const TScalar& radius );
+
+        /// Gets the current radius of the sphere-shaped collider|drawable
+        ///
+        /// @return Radius of the sphere-shaped collider|drawable
+        TScalar radius() const { return m_Radius; }
+
+    private :
+
+        /// Radius of the sphere-shaped collider|drawable
+        TScalar m_Radius;
+    };
+
+    class TCylinder : public TKinematicTreeBody
+    {
+    public :
+
+        TCylinder( const std::string& name,
+                   const TScalar& radius,
+                   const TScalar& height,
+                   const int& collision_group = 1,
+                   const int& collision_mask = 1 );
+
+        TCylinder( const TCylinder& other ) = delete;
+
+        TCylinder& operator=( const TCylinder& other ) = delete;
+
+        ~TCylinder() = default;
+
+        /// Sets the radius of the cylinder-shaped collider|drawable
+        ///
+        /// @param radius   Radius of the cylinder-shaped collider|drawable
+        void SetRadius( const TScalar& radius );
+
+        /// Sets the height of the cylinder-shaped collider|drawable
+        ///
+        /// @param height   Heights of the cylinder-shaped collider|drawable
+        void SetHeight( const TScalar& height );
+
+        /// Gets the radius of the cylinder-shaped collider|drawable
+        ///
+        /// @return Radius of the cylinder-shaped collider|drawable
+        TScalar radius() const { return m_Radius; }
+
+        /// Gets the heights of the cylinder-shapes collider|drawable
+        ///
+        /// @return Height of the cylinder-shaped collider|drawable
+        TScalar height() const { return m_Height; }
+
+    private :
+
+        /// Radius of the cylinder-shaped collider|drawable
+        TScalar m_Radius;
+        /// Height of the cylinder-shaped collider|drawable
+        TScalar m_Height;
+    };
+
+    class TCapsule : public TKinematicTreeBody
+    {
+    public :
+
+        TCapsule( const std::string& name,
+                  const TScalar& radius,
+                  const TScalar& height,
+                  const int& collision_group = 1,
+                  const int& collision_mask = 1 );
+
+        TCapsule( const TCapsule& other ) = delete;
+
+        TCapsule& operator=( const TCapsule& other ) = delete;
+
+        ~TCapsule() = default;
+
+        /// Sets the radius of the capsule-shaped collider|drawable
+        ///
+        /// @param radius   Radius of the capsule-shaped collider|drawable
+        void SetRadius( const TScalar& radius );
+
+        /// Sets the height of the capsule-shaped collider|drawable
+        ///
+        /// @param height   Heights of the capsule-shaped collider|drawable
+        void SetHeight( const TScalar& height );
+
+        /// Gets the radius of the capsule-shaped collider|drawable
+        ///
+        /// @return Radius of the capsule-shaped collider|drawable
+        TScalar radius() const { return m_Radius; }
+
+        /// Gets the height of the capsule-shaped collider|drawable
+        ///
+        /// @return Height of the capsule-shaped collider|drawable
+        TScalar height() const { return m_Height; }
+
+    private :
+
+        /// Radius of the capsule-shaped collider|drawable
+        TScalar m_Radius;
+        /// Height of the capsule-shaped collider|drawable
+        TScalar m_Height;
+    };
+
+    class TEllipsoid : public TKinematicTreeBody
+    {
+    public :
+
+        TEllipsoid( const std::string& name,
+                    const TVec3& radii,
+                    const int& collision_group = 1,
+                    const int& collision_mask = 1 );
+
+        TEllipsoid( const TEllipsoid& other ) = delete;
+
+        TEllipsoid& operator=( const TEllipsoid& other ) = delete;
+
+        ~TEllipsoid() = default;
+
+        /// Sets the radii (x,y,z radius) of the ellipsoid-shaped collider|drawable
+        ///
+        /// @param radii    Radii (vector-3 with x,y,z radius) of the ellipsoid-shaped collider|drawable
+        void SetRadii( const TVec3& radii );
+
+        /// Sets the x-radius of the ellipsoid-shaped collider|drawable
+        ///
+        /// @param radius_x X-radius of the ellipsoid-shaped collider|drawable
+        void SetRadiusX( const TScalar& radius_x );
+
+        /// Sets the y-radius of the ellipsoid-shaped collider|drawable
+        ///
+        /// @param radius_y Y-radius of the ellipsoid-shaped collider|drawable
+        void SetRadiusY( const TScalar& radius_y );
+
+        /// Sets the z-radius of the ellipsoid-shaped collider|drawable
+        ///
+        /// @param radius_z Z-radius of the ellipsoid-shaped collider|drawable
+        void SetRadiusZ( const TScalar& radius_z );
+
+        /// Gets the radii of the ellipsoid-shaped collider|drawable
+        ///
+        /// @return Radii (vector-3 with x,y,z radius) of the ellipsoid-shaped collider|drawable
+        TVec3 radii() const { return m_Radii; }
+
+        /// Gets the x-radius of the ellipsoid-shaped collider|drawable
+        ///
+        /// @return X-radius of the ellipsoid-shaped collider|drawable
+        TScalar radius_x() const { return m_Radii.x(); }
+
+        /// Gets the y-radius of the ellipsoid-shaped collider|drawable
+        ///
+        /// @return Y-radius of the ellipsoid-shaped collider|drawable
+        TScalar radius_y() const { return m_Radii.y(); }
+
+        /// Gets the z-radius of the ellipsoid-shaped collider|drawable
+        ///
+        /// @return Z-radius of the ellipsoid-shaped collider|drawable
+        TScalar radius_z() const { return m_Radii.z(); }
+
+    private :
+
+        TVec3 m_Radii;
+    };
+
+    class TConvexMesh : public TKinematicTreeBody
+    {
+    public :
+
+        TConvexMesh( const std::string& name,
+                     const std::string& mesh_collider_filepath,
+                     const std::string& mesh_visual_filepath,
+                     const TScalar& mesh_scale,
+                     const int& collision_group = 1,
+                     const int& collision_mask = 1 );
+
+        TConvexMesh( const TConvexMesh& other ) = delete;
+
+        TConvexMesh& operator=( const TConvexMesh& other ) = delete;
+
+        ~TConvexMesh() = default;
+
+        /// Sets the scale of the mesh collider|drawable
+        ///
+        /// @param scale    Scale factor applied to the mesh collider|drawable
+        void SetScale( const TScalar& scale );
+
+        /// Gets the scale of the mesh collider|drawable
+        ///
+        /// @return Scale factor of the mesh collider|drawable
+        TScalar scale() const { return m_Scale; }
+
+    private :
+
+        /// Scale of the mesh collider|drawable
+        TScalar m_Scale;
     };
 }}
--- src/kinematic_trees/loco_kinematic_tree_body.cpp
+++ src/kinematic_trees/loco_kinematic_tree_body.cpp
@@ -4,15 +4,28 @@
 namespace loco {
 namespace kintree {
 
+    //--------------------------------------------------------------------------------------------//
+    //                                                                                            //
+    //                               Kinematic-Tree-Body Interface                                //
+    //                                                                                            //
+    //--------------------------------------------------------------------------------------------//
+
+    TKinematicTreeBody::TKinematicTreeBody( const std::string& name )
+        : TObject( name )
+    {
+    #if defined(LOCO_CORE_USE_TRACK_ALLOCS)
+        if ( tinyutils::Logger::IsActive() )
+            LOCO_CORE_TRACE( "Loco::Allocs: Created TKinematicTreeBody {0} @ {1} ", m_name, tinyutils::PointerToHexAddress( this ) );
+        else
+            std::cout << "Loco::Allocs: Created TKinematicTreeBody " << m_name << " @ " << tinyutils::PointerToHexAddress( this );
+    #endif
+    }
+
     TKinematicTreeBody::TKinematicTreeBody( const std::string& name,
-                                            const TKinematicTreeBodyData& body_data,
-                                            const TVec3& position,
-                                            const TMat3& rotation )
+                                            const TKinematicTreeBodyData& body_data )
         : TObject( name )
     {
         m_Data = body_data;
-        m_tf.set( position, 3 );
-        m_tf.set( rotation );
 
         for ( auto collider_data : body_data.colliders )
         {
@@ -323,5 +336,321 @@ namespace kintree {
                         this functionality. Change the transform of the parent body instead. (body={0})", m_name );
         if ( m_ParentBodyRef )
             m_tf = m_ParentBodyRef->tf() * m_LocalTf;
+    }
+
+    //--------------------------------------------------------------------------------------------//
+    //                                                                                            //
+    //                              Kinematic-Tree-Body Primitives                                //
+    //                                                                                            //
+    //--------------------------------------------------------------------------------------------//
+
+    //********************************************************************************************//
+    //                                Box primitive Implementation                                //
+    //********************************************************************************************//
+
+    TBox::TBox( const std::string& name,
+                const TVec3& extents,
+                const int& collision_group,
+                const int& collision_mask )
+        : TKinematicTreeBody( name )
+    {
+        auto collider_data = TCollisionData();
+        collider_data.type = eShapeType::BOX;
+        collider_data.size = extents;
+        collider_data.collisionGroup = collision_group;
+        collider_data.collisionMask = collision_mask;
+        m_Colliders.push_back( std::make_unique<TKinematicTreeCollider>( name + loco::SUFFIX_COLLIDER, collider_data ) );
+        m_Colliders.back()->SetParentBody( this, TMat4() );
+
+        auto visual_data = TVisualData();
+        visual_data.type = eShapeType::BOX;
+        visual_data.size = extents;
+        visual_data.ambient = loco::DEFAULT_AMBIENT_COLOR;
+        visual_data.diffuse = loco::DEFAULT_DIFFUSE_COLOR;
+        visual_data.specular = loco::DEFAULT_SPECULAR_COLOR;
+        visual_data.shininess = loco::DEFAULT_SHININESS;
+        m_Drawables.push_back( std::make_unique<visualizer::TDrawable>( name + loco::SUFFIX_DRAWABLE, visual_data ) );
+        m_Drawables.back()->SetParentObject( this );
+        m_Drawables.back()->SetLocalTransform( TMat4() );
+
+        m_Data.colliders.push_back( collider_data );
+        m_Data.drawables.push_back( visual_data );
+        m_Extents = extents;
+    }
+
+    void TBox::SetExtents( const TVec3& extents )
+    {
+        m_Extents = extents;
+        m_Colliders.back()->ChangeSize( m_Extents );
+        m_Drawables.back()->ChangeSize( m_Extents );
+    }
+
+    void TBox::SetWidth( const TScalar& width )
+    {
+        m_Extents.x() = width;
+        m_Colliders.back()->ChangeSize( m_Extents );
+        m_Drawables.back()->ChangeSize( m_Extents );
+    }
+
+    void TBox::SetDepth( const TScalar& depth )
+    {
+        m_Extents.y() = depth;
+        m_Colliders.back()->ChangeSize( m_Extents );
+        m_Drawables.back()->ChangeSize( m_Extents );
+    }
+
+    void TBox::SetHeight( const TScalar& height )
+    {
+        m_Extents.z() = height;
+        m_Colliders.back()->ChangeSize( m_Extents );
+        m_Drawables.back()->ChangeSize( m_Extents );
+    }
+
+    //********************************************************************************************//
+    //                               Sphere primitive Implementation                              //
+    //********************************************************************************************//
+
+    TSphere::TSphere( const std::string& name,
+                      const TScalar& radius,
+                      const int& collision_group,
+                      const int& collision_mask )
+        : TKinematicTreeBody( name )
+    {
+        auto collider_data = TCollisionData();
+        collider_data.type = eShapeType::SPHERE;
+        collider_data.size = { radius, 0.0f, 0.0f };
+        collider_data.collisionGroup = collision_group;
+        collider_data.collisionMask = collision_mask;
+        m_Colliders.push_back( std::make_unique<TKinematicTreeCollider>( name + loco::SUFFIX_COLLIDER, collider_data ) );
+        m_Colliders.back()->SetParentBody( this, TMat4() );
+
+        auto visual_data = TVisualData();
+        visual_data.type = eShapeType::SPHERE;
+        visual_data.size = { radius, 0.0f, 0.0f };
+        visual_data.ambient = loco::DEFAULT_AMBIENT_COLOR;
+        visual_data.diffuse = loco::DEFAULT_DIFFUSE_COLOR;
+        visual_data.specular = loco::DEFAULT_SPECULAR_COLOR;
+        visual_data.shininess = loco::DEFAULT_SHININESS;
+        m_Drawables.push_back( std::make_unique<visualizer::TDrawable>( name + loco::SUFFIX_DRAWABLE, visual_data ) );
+        m_Drawables.back()->SetParentObject( this );
+        m_Drawables.back()->SetLocalTransform( TMat4() );
+
+        m_Data.colliders.push_back( collider_data );
+        m_Data.drawables.push_back( visual_data );
+        m_Radius = radius;
+    }
+
+    void TSphere::SetRadius( const TScalar& radius )
+    {
+        m_Radius = radius;
+        m_Colliders.back()->ChangeSize( { m_Radius, 0.0f, 0.0f } );
+        m_Drawables.back()->ChangeSize( { m_Radius, 0.0f, 0.0f } );
+    }
+
+    //********************************************************************************************//
+    //                              Cylinder primitive Implementation                             //
+    //********************************************************************************************//
+
+    TCylinder::TCylinder( const std::string& name,
+                          const TScalar& radius,
+                          const TScalar& height,
+                          const int& collision_group,
+                          const int& collision_mask )
+        : TKinematicTreeBody( name )
+    {
+        auto collider_data = TCollisionData();
+        collider_data.type = eShapeType::CYLINDER;
+        collider_data.size = { radius, height, 0.0f };
+        collider_data.collisionGroup = collision_group;
+        collider_data.collisionMask = collision_mask;
+        m_Colliders.push_back( std::make_unique<TKinematicTreeCollider>( name + loco::SUFFIX_COLLIDER, collider_data ) );
+        m_Colliders.back()->SetParentBody( this, TMat4() );
+
+        auto visual_data = TVisualData();
+        visual_data.type = eShapeType::CYLINDER;
+        visual_data.size = { radius, height, 0.0f };
+        visual_data.ambient = loco::DEFAULT_AMBIENT_COLOR;
+        visual_data.diffuse = loco::DEFAULT_DIFFUSE_COLOR;
+        visual_data.specular = loco::DEFAULT_SPECULAR_COLOR;
+        visual_data.shininess = loco::DEFAULT_SHININESS;
+        m_Drawables.push_back( std::make_unique<visualizer::TDrawable>( name + loco::SUFFIX_DRAWABLE, visual_data ) );
+        m_Drawables.back()->SetParentObject( this );
+        m_Drawables.back()->SetLocalTransform( TMat4() );
+
+        m_Data.colliders.push_back( collider_data );
+        m_Data.drawables.push_back( visual_data );
+        m_Radius = radius;
+        m_Height = height;
+    }
+
+    void TCylinder::SetRadius( const TScalar& radius )
+    {
+        m_Radius = radius;
+        m_Colliders.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+        m_Drawables.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+    }
+
+    void TCylinder::SetHeight( const TScalar& height )
+    {
+        m_Height = height;
+        m_Colliders.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+        m_Drawables.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+    }
+
+    //********************************************************************************************//
+    //                              Capsule primitive Implementation                              //
+    //********************************************************************************************//
+
+    TCapsule::TCapsule( const std::string& name,
+                        const TScalar& radius,
+                        const TScalar& height,
+                        const int& collision_group,
+                        const int& collision_mask )
+        : TKinematicTreeBody( name )
+    {
+        auto collider_data = TCollisionData();
+        collider_data.type = eShapeType::CAPSULE;
+        collider_data.size = { radius, height, 0.0f };
+        collider_data.collisionGroup = collision_group;
+        collider_data.collisionMask = collision_mask;
+        m_Colliders.push_back( std::make_unique<TKinematicTreeCollider>( name + loco::SUFFIX_COLLIDER, collider_data ) );
+        m_Colliders.back()->SetParentBody( this, TMat4() );
+
+        auto visual_data = TVisualData();
+        visual_data.type = eShapeType::CAPSULE;
+        visual_data.size = { radius, height, 0.0f };
+        visual_data.ambient = loco::DEFAULT_AMBIENT_COLOR;
+        visual_data.diffuse = loco::DEFAULT_DIFFUSE_COLOR;
+        visual_data.specular = loco::DEFAULT_SPECULAR_COLOR;
+        visual_data.shininess = loco::DEFAULT_SHININESS;
+        m_Drawables.push_back( std::make_unique<visualizer::TDrawable>( name + loco::SUFFIX_DRAWABLE, visual_data ) );
+        m_Drawables.back()->SetParentObject( this );
+        m_Drawables.back()->SetLocalTransform( TMat4() );
+
+        m_Data.colliders.push_back( collider_data );
+        m_Data.drawables.push_back( visual_data );
+        m_Radius = radius;
+        m_Height = height;
+    }
+
+    void TCapsule::SetRadius( const TScalar& radius )
+    {
+        m_Radius = radius;
+        m_Colliders.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+        m_Drawables.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+    }
+
+    void TCapsule::SetHeight( const TScalar& height )
+    {
+        m_Height = height;
+        m_Colliders.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+        m_Drawables.back()->ChangeSize( { m_Radius, m_Height, 0.0f } );
+    }
+
+    //********************************************************************************************//
+    //                            Ellipsoid primitive Implementation                              //
+    //********************************************************************************************//
+
+    TEllipsoid::TEllipsoid( const std::string& name,
+                            const TVec3& radii,
+                            const int& collision_group,
+                            const int& collision_mask )
+        : TKinematicTreeBody( name )
+    {
+        auto collider_data = TCollisionData();
+        collider_data.type = eShapeType::ELLIPSOID;
+        collider_data.size = radii;
+        collider_data.collisionGroup = collision_group;
+        collider_data.collisionMask = collision_mask;
+        m_Colliders.push_back( std::make_unique<TKinematicTreeCollider>( name + loco::SUFFIX_COLLIDER, collider_data ) );
+        m_Colliders.back()->SetParentBody( this, TMat4() );
+
+        auto visual_data = TVisualData();
+        visual_data.type = eShapeType::ELLIPSOID;
+        visual_data.size = radii;
+        visual_data.ambient = loco::DEFAULT_AMBIENT_COLOR;
+        visual_data.diffuse = loco::DEFAULT_DIFFUSE_COLOR;
+        visual_data.specular = loco::DEFAULT_SPECULAR_COLOR;
+        visual_data.shininess = loco::DEFAULT_SHININESS;
+        m_Drawables.push_back( std::make_unique<visualizer::TDrawable>( name + loco::SUFFIX_DRAWABLE, visual_data ) );
+        m_Drawables.back()->SetParentObject( this );
+        m_Drawables.back()->SetLocalTransform( TMat4() );
+
+        m_Data.colliders.push_back( collider_data );
+        m_Data.drawables.push_back( visual_data );
+        m_Radii = radii;
+    }
+
+    void TEllipsoid::SetRadii( const TVec3& radii )
+    {
+        m_Radii = radii;
+        m_Colliders.back()->ChangeSize( m_Radii );
+        m_Drawables.back()->ChangeSize( m_Radii );
+    }
+
+    void TEllipsoid::SetRadiusX( const TScalar& radius_x )
+    {
+        m_Radii.x() = radius_x;
+        m_Colliders.back()->ChangeSize( m_Radii );
+        m_Drawables.back()->ChangeSize( m_Radii );
+    }
+
+    void TEllipsoid::SetRadiusY( const TScalar& radius_y )
+    {
+        m_Radii.y() = radius_y;
+        m_Colliders.back()->ChangeSize( m_Radii );
+        m_Drawables.back()->ChangeSize( m_Radii );
+    }
+
+    void TEllipsoid::SetRadiusZ( const TScalar& radius_z )
+    {
+        m_Radii.z() = radius_z;
+        m_Colliders.back()->ChangeSize( m_Radii );
+        m_Drawables.back()->ChangeSize( m_Radii );
+    }
+
+    //********************************************************************************************//
+    //                               Mesh primitive Implementation                                //
+    //********************************************************************************************//
+
+    TConvexMesh::TConvexMesh( const std::string& name,
+                              const std::string& mesh_collider_filepath,
+                              const std::string& mesh_visual_filepath,
+                              const TScalar& mesh_scale,
+                              const int& collision_group,
+                              const int& collision_mask )
+        : TKinematicTreeBody( name )
+    {
+        auto collider_data = TCollisionData();
+        collider_data.type = eShapeType::MESH;
+        collider_data.size = { mesh_scale, mesh_scale, mesh_scale };
+        collider_data.mesh_data.filename = mesh_collider_filepath;
+        collider_data.collisionGroup = collision_group;
+        collider_data.collisionMask = collision_mask;
+        m_Colliders.push_back( std::make_unique<TKinematicTreeCollider>( name + loco::SUFFIX_COLLIDER, collider_data ) );
+        m_Colliders.back()->SetParentBody( this, TMat4() );
+
+        auto visual_data = TVisualData();
+        visual_data.type = eShapeType::MESH;
+        visual_data.size = { mesh_scale, mesh_scale, mesh_scale };
+        visual_data.mesh_data.filename = mesh_visual_filepath;
+        visual_data.ambient = loco::DEFAULT_AMBIENT_COLOR;
+        visual_data.diffuse = loco::DEFAULT_DIFFUSE_COLOR;
+        visual_data.specular = loco::DEFAULT_SPECULAR_COLOR;
+        visual_data.shininess = loco::DEFAULT_SHININESS;
+        m_Drawables.push_back( std::make_unique<visualizer::TDrawable>( name + loco::SUFFIX_DRAWABLE, visual_data ) );
+        m_Drawables.back()->SetParentObject( this );
+        m_Drawables.back()->SetLocalTransform( TMat4() );
+
+        m_Data.colliders.push_back( collider_data );
+        m_Data.drawables.push_back( visual_data );
+        m_Scale = mesh_scale;
+    }
+
+    void TConvexMesh::SetScale( const TScalar& scale )
+    {
+        m_Scale = scale;
+        m_Colliders.back()->ChangeSize( { m_Scale, m_Scale, m_Scale } );
+        m_Drawables.back()->ChangeSize( { m_Scale, m_Scale, m_Scale } );
     }
 }}
