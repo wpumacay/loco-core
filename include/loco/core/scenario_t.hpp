5633d2bc16ebc0e8fb0eed18abd4865e8391a5bd
gregor <wpumacay@gmail.com>
2023-09-25 09:09:20 -0500

    (feat): update scenario_t API

--- include/loco/core/scenario_t.hpp
+++ include/loco/core/scenario_t.hpp
@@ -2,13 +2,16 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 
-#include "./common.hpp"
-#include "./body_t.hpp"
+#include <loco/core/common.hpp>
+#include <loco/core/single_body/single_body_t.hpp>
+#include <loco/core/visualizer/drawable_t.hpp>
 
 namespace loco {
 namespace core {
 
+/// \brief Representation for the main container of simulated objects
 class Scenario {
     // cppcheck-suppress unknownMacro
     NO_COPY_NO_MOVE_NO_ASSIGN(Scenario)
@@ -17,7 +20,15 @@ class Scenario {
 
  public:
     /// Max number of bodies allowed in a single scene (used for array capacity)
-    static constexpr size_t MAX_BODIES = 128;
+    static constexpr size_t MAX_BODIES = 1024;
+    /// Max number of free drawables allowed in a single scene
+    static constexpr size_t MAX_FREE_DRAWABLES = 1024;
+
+    /// The number of single bodies created/added so far to the scene
+    static size_t s_bodies_so_far;  // NOLINT
+
+    /// The number of free drawables created/added so far to the scene
+    static size_t s_drawables_so_far;  // NOLINT
 
     /// Creates a scenario with a default dummy runtime and backend
     Scenario();
@@ -25,18 +36,44 @@ class Scenario {
     /// Releases/Frees all allocated resources of this scenario
     ~Scenario();
 
+    /// \brief Adds the given body to the scenario with the given name
+    ///
+    /// \param[in] body The body we want to add to the scenario
+    /// \param[in] name The name of the body in the scenario
+    auto AddSingleBody(SingleBody::ptr body, const std::string& name = "")
+        -> void;
+
+    /// \brief Adds a given free drawable to the scenario with the given name
+    ///
+    /// \param[in] drawable The drawable we want to add to the scenario
+    /// \param[in] name The name of the drawable in the scenario
+    auto AddFreeDrawable(Drawable::ptr drawable, const std::string& name = "")
+        -> void;
+
     /// Returns the current number of bodies in this scenario
-    auto num_bodies() const -> size_t { return this->bodies.size(); }
+    auto num_single_bodies() const -> size_t;
+
+    /// Returns the current number of free drawables in this scenario
+    auto num_free_drawables() const -> size_t;
 
     /// Returns whether this scenario is empty or not
-    auto empty() const -> bool { return this->bodies.empty(); }
+    auto empty() const -> bool;
 
     /// Returns a string representation of this scenario
     auto toString() const -> std::string;
 
- public:
+ protected:
     /// The list of bodies currently hold by this scenario
-    std::vector<Body::ptr> bodies;
+    std::vector<SingleBody::ptr> m_SingleBodies;
+
+    /// The list of free drawables hold by this scenario
+    std::vector<Drawable::ptr> m_FreeDrawables;
+
+    /// The keymap used to link bodies by name to its location in the list
+    std::unordered_map<std::string, int> m_SingleBodiesKeymap;
+
+    /// The keympa used to link drawables by name to its location in the list
+    std::unordered_map<std::string, int> m_FreeDrawablesKeymap;
 };
 
 }  // namespace core
--- source/loco/core/scenario_t.cpp
+++ source/loco/core/scenario_t.cpp
@@ -7,15 +7,66 @@
 namespace loco {
 namespace core {
 
-constexpr size_t Scenario::MAX_BODIES;
+size_t Scenario::s_bodies_so_far = 0;     // NOLINT
+size_t Scenario::s_drawables_so_far = 0;  // NOLINT
 
-Scenario::Scenario() { this->bodies.reserve(Scenario::MAX_BODIES); }
+Scenario::Scenario() {
+    m_SingleBodies.reserve(Scenario::MAX_BODIES);
+    m_FreeDrawables.reserve(Scenario::MAX_FREE_DRAWABLES);
+}
 
-Scenario::~Scenario() { this->bodies.clear(); }
+Scenario::~Scenario() {
+    m_SingleBodies.clear();
+    m_FreeDrawables.clear();
+}
+
+auto Scenario::AddSingleBody(SingleBody::ptr body, const std::string& name)
+    -> void {
+    auto body_name =
+        (name.empty())
+            ? "single_body_" + std::to_string(Scenario::s_bodies_so_far)
+            : name;
+    m_SingleBodies.push_back(std::move(body));
+    m_SingleBodiesKeymap[body_name] =
+        static_cast<int>(m_SingleBodies.size()) - 1;
+}
+
+auto Scenario::AddFreeDrawable(Drawable::ptr drawable, const std::string& name)
+    -> void {
+    auto drawable_name =
+        (name.empty())
+            ? "free_drawable_" + std::to_string(Scenario::s_drawables_so_far)
+            : name;
+    m_FreeDrawables.push_back(std::move(drawable));
+    m_FreeDrawablesKeymap[drawable_name] =
+        static_cast<int>(m_FreeDrawables.size()) - 1;
+}
+
+auto Scenario::num_single_bodies() const -> size_t {
+    return m_SingleBodies.size();
+}
+
+auto Scenario::num_free_drawables() const -> size_t {
+    return m_FreeDrawables.size();
+}
+
+auto Scenario::empty() const -> bool {
+    return m_SingleBodies.empty() && m_FreeDrawables.empty();
+}
 
 auto Scenario::toString() const -> std::string {
-    return fmt::format("Scenario(num_bodies={}, max_bodies={})", num_bodies(),
-                       Scenario::MAX_BODIES);
+    return fmt::format(
+        "<Scenario\n"
+        "  num_single_bodies={}\n"
+        "  num_free_drawables={}\n"
+        "  num_single_bodies_so_far={}\n"
+        "  num_free_drawables_so_far={}\n"
+        "  max_single_bodies={}\n"
+        "  max_free_drawables={}\n"
+        ">",
+        m_SingleBodies.size(), m_FreeDrawables.size(),
+        Scenario::s_bodies_so_far, Scenario::s_drawables_so_far,
+        Scenario::MAX_BODIES, Scenario::MAX_FREE_DRAWABLES);
 }
 
 }  // namespace core
