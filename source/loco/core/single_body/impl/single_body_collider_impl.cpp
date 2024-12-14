a27fde749026ed10f20bd6c3b4a1ca27023077cb
wpumacay <wpumacay@gmail.com>
2024-12-14 00:23:09 -0500

    (chore): clearning some compile warnings

--- source/loco/core/single_body/impl/single_body_collider_impl.cpp
+++ source/loco/core/single_body/impl/single_body_collider_impl.cpp
@@ -1,6 +1,9 @@
 #if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-parameter"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #endif
 
 #include <loco/core/single_body/impl/single_body_collider_impl.hpp>
@@ -38,4 +41,6 @@ auto SingleBodyColliderImplNone::ChangeCollisionMask(int32_t col_mask) -> void {
 
 #if defined(__clang__)
 #pragma clang diagnostic pop  // NOLINT
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop  // NOLINT
 #endif
